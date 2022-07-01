cd120b69b29a0b33e1a65cbe70377db905fc9f80
manohar mittapally <manohar_mittapally@epam.com>
2022-07-01 21:15:42 +0300

    added question

--- Java/src/main/java/basics/impl/Dice.java
+++ Java/src/main/java/basics/impl/Dice.java
@@ -3,6 +3,12 @@ package basics.impl;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ *  Write an interactive dice throwing program. The program should ask the user for
+ * a number of D6s to use, and prints its result on the screen. Use the
+ * java.util.Random and java.io.Console classes for the implementation. The code
+ * should run until the user specifies the empty string.
+ */
 public class Dice {
     public void play(){
         Scanner scanner=new Scanner(System.in);
